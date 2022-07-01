96e8200647efc6d2c807a0aff0da7a3b95ef6678
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-01 18:40:29 -0300

    :bug: fix(validator function): remove uncessary characters from the error message

--- services/validators/index.js
+++ services/validators/index.js
@@ -2,7 +2,7 @@ function validateData(schema) {
   return (data) => {
     const { error, value } = schema.validate(data);
     if (error) {
-      error.message = error.details[0].message;
+      error.message = error.details[0].message.replace(/\[[0-9]*\]\./, '');
       throw error;
     }
     return value;
