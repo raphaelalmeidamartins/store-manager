5d0842bf72d7d4ce7a220d4d85a6ee660cf0cdce
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-30 21:22:20 -0300

    :sparkles: feat(routes and middlewares):
    Added products routes, json and error middlewares

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const productsRoutes = require('./routes/productsRoutes');
 
 const app = express();
 
@@ -11,6 +12,23 @@ app.get('/', (_request, response) => {
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
 
-// começando projeto
+app.use(express.json());
+
+app.use('/products', productsRoutes);
+
+app.use((err, _req, res, _next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    default:
+      console.warn(err);
+      res.sendStatus(500);
+  }
+});
 
 module.exports = app;
